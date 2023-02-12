e8c7adb58e63cd9424d4467241e8908c887a67da
Patryk <patryk.ciwinski@gmail.com>
2023-02-12 10:05:39 +0100

    boring

--- 1_zajecia.py
+++ 1_zajecia.py
@@ -1,1 +1,12 @@
 print("hello world")
+word= input("podaj słowo: ")
+def nothing_special(word):
+    output_text=''
+    for letter in word:
+        if letter in ['a','e','i','o','y', 'u']:
+            output_text=''.join([output_text, "*"])
+        else:
+            output_text = ''.join([output_text,letter])
+    return output_text
+
+print(nothing_special(word))
